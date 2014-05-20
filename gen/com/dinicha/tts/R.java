3c4662f9c6f8283f3788e1d418bbbf216045f265
admin@djogalz.com <admin@djogalz.com@e0f1135d-ccbe-4da1-92b3-6f21c471858c>
2014-05-20 14:26:47 +0000

    git-svn-id: https://pl6.projectlocker.com/dnctts_com/DNCTTS/svn@15 e0f1135d-ccbe-4da1-92b3-6f21c471858c

--- gen/com/dinicha/tts/R.java
+++ gen/com/dinicha/tts/R.java
@@ -61,7 +61,6 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090017;
         public static final int baseLayout=0x7f090007;
         public static final int blankImage=0x7f090010;
         public static final int btnExit=0x7f090004;
@@ -69,9 +68,9 @@ containing a value of this type.
         public static final int btnMain=0x7f090002;
         public static final int crosswordLayout=0x7f09000d;
         public static final int guess=0x7f090013;
-        public static final int menu_horizontal=0x7f090015;
-        public static final int menu_settings=0x7f090014;
-        public static final int menu_vertical=0x7f090016;
+        public static final int menu_horizontal=0x7f090014;
+        public static final int menu_settings=0x7f090016;
+        public static final int menu_vertical=0x7f090015;
         public static final int number=0x7f090012;
         public static final int question=0x7f09000a;
         public static final int questionLayout=0x7f090009;
@@ -102,21 +101,20 @@ containing a value of this type.
         public static final int splash=0x7f080004;
     }
     public static final class string {
-        public static final int action_settings=0x7f06000e;
+        public static final int action_settings=0x7f06000d;
         public static final int app_name=0x7f060000;
         public static final int dummy_button=0x7f060004;
         public static final int dummy_content=0x7f060003;
         public static final int hello_world=0x7f060005;
         public static final int menu_horizontal=0x7f060001;
-        public static final int menu_settings=0x7f060006;
         public static final int menu_vertical=0x7f060002;
-        public static final int title_activity_help=0x7f06000d;
-        public static final int title_activity_list=0x7f06000c;
-        public static final int title_activity_main=0x7f060007;
-        public static final int title_activity_test=0x7f06000b;
-        public static final int title_section1=0x7f06000a;
-        public static final int title_section2=0x7f060009;
-        public static final int title_section3=0x7f060008;
+        public static final int title_activity_help=0x7f06000c;
+        public static final int title_activity_list=0x7f06000b;
+        public static final int title_activity_main=0x7f060006;
+        public static final int title_activity_test=0x7f06000a;
+        public static final int title_section1=0x7f060009;
+        public static final int title_section2=0x7f060008;
+        public static final int title_section3=0x7f060007;
     }
     public static final class style {
         /** 
